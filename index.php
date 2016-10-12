d67010e57bf6f0ff1af0386f49fd8b095e491f0f
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-12 14:02:22 -0600

    use case

--- index.php
+++ index.php
@@ -2,16 +2,26 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+
+		<!--beginning -->
 		<title>
 			Nastygal Data site
 		</title>
+		<!-- image before heading-->
 	</head>
 	<body>
+
 		<!--this is the persona section -->
 		<img src="img/nastygalresized.jpg" alt="thenastygal">
 		<h1>The NastyGal &#10084</h1>
+
 		<p> A Nasty Gal is trendy and vintage is her passion! She &#10084 all things quirky, fashion foward and most important unique! Her worst nightmare is to go into a room and find another gal with the same outfit as her. Age isn't a limitation for a Nasty Gal after all trendy fashion never goes out of style in her eyes, it simply evolves. A Nasty Gal will do anything for her unique one of a kind pieces; from working long hours at a job she absolutely hates to saving up for months just to buy a specific pair of shoes! Hey who could blame her for coveting those totally awesome Jeffery Campbell platform sandals sold only on Nastygal.com. Everyone wants to be a NastyGal she is fearless and sure of exactly what she does and doesnt like. The most fustrating part of the NastyGal's life is finding her one of a kind items; until she found NastyGal. </p>
-		<!-- this is the Use Case Section-->
+
+		<!-- Use Case Section-->
+		<section>
 		<h2> When Magic happens </h2>
+			<p> Often time the Nasty Gal finds the most incredible pieces. What excitment this brings, then there comes the delimma! What will I wear with this. She needs to simply find styling inspiration quickly. There is no better way than to visit the Nasty Galaxy section on Nastgal.com. This is where the currators of all the pieces are able to talk about the inspiration behind how they styled these unique one of a kind pieces. The Nasty Gal can also get what are often crazy funny and unique stories behind acquiring and item too. Oh the things we here at Nasty Gal goes through to offer the best of the best to you Gals' out there. After a Nasty Gal has recieved her package she has waited no more than 5-7 days for. After a long day of work or school she
+			</p>
+		</section>
 	</body>
 </html>
