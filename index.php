a24f6cf51965c25486adf52bb2710568e801a139
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-12 12:53:50 -0600

    added persona changed persaon heading

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>
+			Nastygal Data site
+		</title>
+	</head>
+	<body>
+		<h1>The NastyGal &#10084</h1>
+		<p> A Nasty Gal is trendy and vitange is her passion! She &#10084 all things quirky, fashion foward and most important unique! Her worst nightmare is to go into a room and find another gal with the same outfit as her. Age isn't a limitation for a Nasty Gal after all trendy fashion never goes out of style in her eyes, it simply evolves. A Nasty Gal will do anything for her unique one of a kind pieces; from working long hours at a job she absolutely hates to saving up for months just to buy a specific pair of shoes! Hey who could blame her for coveting those totally awesome Jeffery Campbell platform sandals sold only on Nastygal.com. Everyone wants to be a NastyGal she is fearless and sure of exactly what she does and doesnt like. </p>
+	</body>
+</html>
