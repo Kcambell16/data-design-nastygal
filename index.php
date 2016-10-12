b189ea522b2ceffac2add1b06430d77ba81679e6
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-12 13:22:20 -0600

    pciture added to to persona

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
 		</title>
 	</head>
 	<body>
+		<!--this is the persona section -->
 		<h1>The NastyGal &#10084</h1>
-		<p> A Nasty Gal is trendy and vitange is her passion! She &#10084 all things quirky, fashion foward and most important unique! Her worst nightmare is to go into a room and find another gal with the same outfit as her. Age isn't a limitation for a Nasty Gal after all trendy fashion never goes out of style in her eyes, it simply evolves. A Nasty Gal will do anything for her unique one of a kind pieces; from working long hours at a job she absolutely hates to saving up for months just to buy a specific pair of shoes! Hey who could blame her for coveting those totally awesome Jeffery Campbell platform sandals sold only on Nastygal.com. Everyone wants to be a NastyGal she is fearless and sure of exactly what she does and doesnt like. </p>
+		<img src="img/nastygalresized.jpg" alt="thenastygal">
+		<p> A Nasty Gal is trendy and vintage is her passion! She &#10084 all things quirky, fashion foward and most important unique! Her worst nightmare is to go into a room and find another gal with the same outfit as her. Age isn't a limitation for a Nasty Gal after all trendy fashion never goes out of style in her eyes, it simply evolves. A Nasty Gal will do anything for her unique one of a kind pieces; from working long hours at a job she absolutely hates to saving up for months just to buy a specific pair of shoes! Hey who could blame her for coveting those totally awesome Jeffery Campbell platform sandals sold only on Nastygal.com. Everyone wants to be a NastyGal she is fearless and sure of exactly what she does and doesnt like. The most fustrating part of the NastyGal's life is finding her one of a kind items; until she found NastyGal. </p>
+		<!-- this is the Use Case Section-->
+		<h2> When Magic happens </h2>
 	</body>
 </html>
