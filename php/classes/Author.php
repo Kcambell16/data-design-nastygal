67c913ec77147714554db019d7a1ad00b7b281d8
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-19 12:03:35 -0600

    object oriented coding start

---
+++ php/classes/Author.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kcampbell
+ * Date: 10/19/16
+ * Time: 11:54 AM
+ **/
+class Author{
+
+
+	/** id for author
+	 * id for this author; this is the primary key
+	 * @var int $authorId
+	 **/
+private $authorId;
+	/**
+	 * id of the author that created this blog post; this is the foreign key
+	 *
+	 **/
+}
+
---
+++ php/classes/Blogpost.php
@@ -0,0 +1,42 @@
+<?php
+/**
+*
+*
+*
+*
+*
+*
+*
+**/
+class blogpost {
+	/**
+	 * id for blog post; this is the primary key
+	 * @var int $blogPostId
+	 **/
+	private $blogPostId;
+	/**
+	 * id of the blog post that created this blog; this the primary key
+	 * @var int
+	 **/
+	private $blogPostContent;
+	/**
+	 *
+	 *
+	 */
+	private $blogPostAuthorId;
+	/**
+	 *
+	 *
+	 **/
+	private $blogPostDate;
+	/**
+	 *
+	 *
+	 **/
+	private $blogPostId;
+	/**
+	 *
+	 *
+	 **/
+}
+?>
