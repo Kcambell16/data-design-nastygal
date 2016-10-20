c8a5c6aff184cebb85929407052da06cbd2d217a
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-20 11:25:30 -0600

    getter methods updated

--- php/classes/Blogpost.php
+++ php/classes/Blogpost.php
@@ -1,42 +1,39 @@
 <?php
+
 /**
-*
-*
-*
-*
-*
-*
-*
-**/
-class blogpost {
+ * Created by PhpStorm.
+ * @author Kelly Campbell <Kcampbell16@cnm.edu>
+ * Date: 10/20/16
+ * Time: 10:07 AM
+ * Typical setup for a blog, stored information on national blog
+ **/
+class Blogpost {
 	/**
 	 * id for blog post; this is the primary key
-	 * @var int $blogPostId
 	 **/
 	private $blogPostId;
 	/**
-	 * id of the blog post that created this blog; this the primary key
-	 * @var int
+	 * id of the blog post that created this blog; this is the primary key
+	 **/
+	private $blogPostAuthorId;
+	/**
+	 *id of the author of the creator of the post; this is the foreign key
 	 **/
 	private $blogPostContent;
 	/**
-	 *
-	 *
-	 */
-	private $blogPostAuthorId;
-	/**
-	 *
-	 *
+	 *  acutal blog post content
 	 **/
 	private $blogPostDate;
 	/**
-	 *
-	 *
+	 * date if blog post
 	 **/
-	private $blogPostId;
+	private $blogPostTitle;
 	/**
-	 *
-	 *
+	 * Actual blog post title
 	 **/
+	/**
+	 * accessor method for blog post id
+	 **/
+
 }
 ?>
---
+++ php/classes/Subscription.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Kcampbell
+ * Date: 10/20/16
+ * Time: 10:07 AM
+ **/
+class Subscription {
+
+}
