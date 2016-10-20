8d777fc74c7ac8dcc40304fbb513b2e52de15117
Kelly Campbell <Kcampbell16@cnm.edu>
2016-10-20 13:26:01 -0600

    mutation method start

--- php/classes/Blogpost.php
+++ php/classes/Blogpost.php
@@ -33,7 +33,32 @@ class Blogpost {
 	 **/
 	/**
 	 * accessor method for blog post id
+	 * @return int value of blog post id
 	 **/
+	public function  getBlogPostId() {
+		return($this->blogPostId);
+	}
+
+	/**
+	 * mutator method for blog post id
+	 *
+	 * @param int $newBlogPostId new value of the blog post id
+	 * @throw UnexpectedValueException if $newBlogPostId is not a integer
+	 **/
+	public function setBlogPostId
+	// verify the blog post id is valid
+	($newBlogPostId) {
+		$newBlogPostId = filter_var($newBlogPostId, FILTER_VALIDATE_INT);
+		IF($newBlogPostId === false) {
+			throw (new UnexpectedValueException("blog post id is not a valid interger"));
+
+
+			// convert and store the blog post id
+			$this->blogPostId - intval($newBlogPostId);
+		}
+
+
+	}
 
 }
-?>
+
